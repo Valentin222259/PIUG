835376cf4bd02d9ff8cb18ca6d1f3113af85ac00
ardeleanvalentin <ardeleanvalentin490@yahoo.com>
2025-11-09 13:28:53 +0200

    .

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -14,10 +14,29 @@ const Home: React.FC = () => {
       <Navbar />
       <div className="flex justify-between items-end mt-[251px] px-[165px]">
         {/* Stânga — text */}
-        <div className="flex flex-col w-[31vw] h-[42vh]">
-          <p>SO, YOU WANT TO TRAVEL TO</p>
-          <h1>SPACE</h1>
-          <p>
+        <div className="flex flex-col w-[31vw] h-[42vh] space-y-[20px]">
+          <p
+            className="m-0 w-full h-[34px] 
+         font-barlowCondensed font-normal 
+         text-[28px] leading-[100%]
+         text-[#D0D6F9]"
+          >
+            SO, YOU WANT TO TRAVEL TO
+          </p>
+          <h1
+            className="m-0 w-full h-[172px]
+         font-bellefair font-normal
+         text-[150px] leading-[100%] 
+         text-[#D0D6F9]"
+          >
+            SPACE
+          </h1>
+          <p
+            className="m-0 w-full h-[128px]
+         font-barlow font-normal
+         text-[18px] leading-[32px]
+         text-[#D0D6F9]"
+          >
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
@@ -28,7 +47,7 @@ const Home: React.FC = () => {
         {/* Dreapta — cercul Explore */}
         <button
           className="top-[55vh] left-[69.5vw]
-    w-[19vw] h-[19vw]
+    w-[18vw] h-[18vw]
     flex items-center justify-center
     rounded-full
     !bg-white text-black
