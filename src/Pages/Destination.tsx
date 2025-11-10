bd74ab1460e0fa340464543a47e3e4786f982387
ardeleanvalentin <ardeleanvalentin490@yahoo.com>
2025-11-10 17:28:05 +0200

    .

--- src/Pages/Destination.tsx
+++ src/Pages/Destination.tsx
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import data from "../../data.json";
 import Navbar from "../Components/Navbar";
 import bgDestination from "../assets/destination/background-destination-desktop.jpg";
+import moonImg from "../assets/destination/image-moon.png";
+import marsImg from "../assets/destination/image-mars.png";
+import europaImg from "../assets/destination/image-europa.png";
+import titanImg from "../assets/destination/image-titan.png";
 
 const Destination: React.FC = () => {
-  const destinations = data.destinations;
+  const destinations = [
+    { ...data.destinations[0], image: moonImg },
+    { ...data.destinations[1], image: marsImg },
+    { ...data.destinations[2], image: europaImg },
+    { ...data.destinations[3], image: titanImg },
+  ];
   const [selected, setSelected] = useState(destinations[0]);
 
   return (
@@ -13,59 +22,82 @@ const Destination: React.FC = () => {
       style={{ backgroundImage: `url(${bgDestination})` }}
     >
       <Navbar />
+      <div
+        className="
+           w-[30.9vw] h-[3.15vh]
+          mt-[16.63vh] ml-[9.67vw]
+          flex items-center gap-[1.8vw]
+        "
+      >
+        <span
+          className="
+            opacity-25 font-['Barlow_Condensed'] font-bold text-[1.46vw] leading-[100%] tracking-[0.25vw] text-white"
+        >
+          01
+        </span>
 
-      <div className="flex justify-between items-center px-[165px] mt-[120px]">
-        {/* Left: Image */}
-        <div className="flex flex-col items-center w-1/2">
-          <h2 className="font-barlowCondensed text-[28px] tracking-[4.72px] text-[#D0D6F9] uppercase mb-[64px]">
-            <span className="opacity-25 mr-[18px]">01</span> Pick your
-            destination
-          </h2>
-          <img
-            src={selected.images.png}
-            alt={selected.name}
-            className="w-[445px] h-[445px] object-contain"
-          />
-        </div>
+        <h2
+          className="font-['Barlow_Condensed'] font-normal
+      text-[1.46vw] leading-[100%] tracking-[0.25vw]
+      uppercase text-white"
+        >
+          Pick your destination
+        </h2>
+      </div>
 
-        {/* Right: Info */}
-        <div className="flex flex-col w-[445px] space-y-[20px]">
+      <div className="flex justify-between items-center w-[80vw] ml-[12vw] mt-[8vh]">
+        <img
+          src={moonImg}
+          alt={selected.name}
+          className="w-[26vw] h-[26vw] object-contain"
+        />
+
+        <div className="w-[30vw] h-auto flex flex-col justify-between -translate-x-[2vw] -translate-y-[1.5vh]">
           {/* Tabs */}
-          <div className="flex gap-[24px] uppercase text-[16px] tracking-[2.7px] font-barlowCondensed">
-            {destinations.map((dest) => (
+          <div className="flex gap-[2vw] mb-[2vh]">
+            {destinations.map((item, index) => (
               <button
-                key={dest.name}
-                onClick={() => setSelected(dest)}
-                className={`pb-[8px] border-b-[3px] transition ${
-                  selected.name === dest.name
-                    ? "border-white text-white"
-                    : "border-transparent text-[#D0D6F9] hover:border-gray-500"
-                }`}
+                key={index}
+                onClick={() => setSelected(item)}
+                className={`font-['Barlow_Condensed'] text-[1vw] uppercase tracking-[0.2vw]
+              pb-[0.5vh] border-b-[3px] transition-all duration-300
+              ${
+                selected.name === item.name
+                  ? "border-white text-white"
+                  : "border-transparent text-[#D0D6F9] hover:border-gray-500 hover:text-white"
+              }`}
               >
-                {dest.name}
+                {item.name}
               </button>
             ))}
           </div>
 
-          <h1 className="font-bellefair text-[100px]">{selected.name}</h1>
-          <p className="font-barlow text-[18px] leading-[32px] text-[#D0D6F9]">
+          <h1 className="font-['Bellefair'] text-[6vw] leading-[100%] uppercase">
+            {selected.name}
+          </h1>
+
+          <p className="font-['Barlow'] text-[1vw] leading-[180%] text-[#D0D6F9] w-[90%]">
             {selected.description}
           </p>
 
-          <hr className="opacity-25 my-[32px]" />
+          <hr className="border-[#383B4B] w-full my-[2vh]" />
 
-          <div className="flex justify-between">
+          <div className="flex justify-between w-[90%]">
             <div>
-              <p className="text-[#D0D6F9] uppercase text-[14px] tracking-[2.36px]">
-                Avg. Distance
+              <p className="font-['Barlow_Condensed'] text-[0.9vw] tracking-[0.17vw] text-[#D0D6F9] uppercase">
+                Avg. distance
               </p>
-              <p className="font-bellefair text-[28px]">{selected.distance}</p>
+              <p className="font-['Bellefair'] text-[1.7vw] uppercase">
+                {selected.distance}
+              </p>
             </div>
             <div>
-              <p className="text-[#D0D6F9] uppercase text-[14px] tracking-[2.36px]">
-                Est. Travel Time
+              <p className="font-['Barlow_Condensed'] text-[0.9vw] tracking-[0.17vw] text-[#D0D6F9] uppercase">
+                Est. travel time
               </p>
-              <p className="font-bellefair text-[28px]">{selected.travel}</p>
+              <p className="font-['Bellefair'] text-[1.7vw] uppercase">
+                {selected.travel}
+              </p>
             </div>
           </div>
         </div>
